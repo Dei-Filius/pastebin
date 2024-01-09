02edb3a23e0092fa2449bce0f49a4c2c6488127e
Dei-Filius <63829092+Dei-Filius@users.noreply.github.com>
2024-01-09 22:49:42 +0800

    Delete static directory

--- static/viewletter.js
+++
@@ -1,7 +0,0 @@
-fetch("http://localhost/letters?id=3").then((res) => {
-    fetch("http://localhost/submit", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message: res.text() }),
-    }).then((res) => console.log(res));
-});
